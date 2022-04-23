97d035314e76e9d910f49c5db0b25c25534d0343
ismael.ramos <ismael.ramos@meta.es>
2022-04-23 11:34:24 +0200

    FEAT(APP): fix login validation

--- src/authentication/dto/login.input.ts
+++ src/authentication/dto/login.input.ts
@@ -19,8 +19,5 @@ export class LoginInput {
   @IsString()
   @MinLength(8)
   @MaxLength(32)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'password is too weak',
-  })
   password: string;
 }

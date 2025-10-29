f6373aa2e412c6d5eee9c9313c302ebc8d176b53
ismaestro <ramossilvanismael@gmail.com>
2025-10-29 16:24:48 +0100

    feat(pokemon): add cache logic to last updated pokemon

--- src/features/pokemon/pokemon.service.ts
+++ src/features/pokemon/pokemon.service.ts
@@ -5,9 +5,25 @@ import { LastUpdatedPokemonResponse } from './dto/last-updated-pokemon.response'
 export class PokemonService {
   private readonly logger = new Logger('UserService');
 
+  private lastResponse: LastUpdatedPokemonResponse | null = null;
+  private lastTimestamp = 0;
+
   async getLastUpdatedPokemonIds(): Promise<LastUpdatedPokemonResponse> {
-    this.logger.log(`[GetLastUpdatedPokemonIds]: return random numbers`);
-    return { pokemonIds: this.getRandomArray() };
+    const now = Date.now();
+
+    const DELAY_SECONDS = 10_000;
+    if (this.lastResponse && now - this.lastTimestamp < DELAY_SECONDS) {
+      this.logger.log('[GetLastUpdatedPokemonIds]: returning cached response');
+      return this.lastResponse;
+    }
+
+    this.logger.log('[GetLastUpdatedPokemonIds]: generating new response');
+    const response = { pokemonIds: this.getRandomArray() };
+
+    this.lastResponse = response;
+    this.lastTimestamp = now;
+
+    return response;
   }
 
   private getRandomArray(): string[] {
